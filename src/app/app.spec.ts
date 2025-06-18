4acd6c5871b243d75f71f35122da024af7f6ef02
Felipe Uribe <wfpaisa@gmail.com>
2025-06-17 22:09:00 -0500

    tailwind

--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -21,6 +21,6 @@ describe('App', () => {
     const fixture = TestBed.createComponent(App);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, pco-boilerplate');
+    expect(compiled.querySelector('h1')?.textContent).toContain('PCO - Boilerplate');
   });
 });
